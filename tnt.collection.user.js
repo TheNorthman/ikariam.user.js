6d4b8840e942f3e29086989a0bf5df2a14510d51
Ronny Jespersen <git@rjj-net.dk>
2024-12-23 17:30:39 +0700

    v1.4.20

--- tnt.collection.user.js
+++ tnt.collection.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         TNT Collection
-// @version      1.4.19
+// @version      1.4.20
 // @namespace    tnt.collection
 // @author       Ronny Jespersen
 // @description  TNT Collection of Ikariam enhancements to enhance the game
@@ -772,8 +772,8 @@ var tnt = {
                 $.each(tnt.data.storage.resources.city, function (index, value) {
                     table += '<tr>\
                         <td class="tnt_city">' + tnt.get.cityName(index) + '</td>\
-                        <td class="tnt_population">' + Math.round(value.population).toLocaleString() + '</td>\
-                        <td class="tnt_citizens">' + Math.round(value.citizens).toLocaleString() + '</td>\
+                        <td class="tnt_population">' + parseInt(Math.round(value.population)).toLocaleString() + '</td>\
+                        <td class="tnt_citizens">' + parseInt(Math.round(value.citizens)).toLocaleString() + '</td>\
                         <td class="tnt_wood">' + value.wood.toLocaleString() + '</td>\
                         <td class="tnt_wine">' + value.wine.toLocaleString() + '</td>\
                         <td class="tnt_marble">' + value.marble.toLocaleString() + '</td>\
@@ -891,6 +891,12 @@ GM_addStyle("\
         border-radius: 50%;\
         border: 1px solid #000;\
         display: inline-block;\
+    }\
+    .tnt_population{\
+        text-align:right;\
+    }\
+    .tnt_citizens{\
+        text-align:right;\
     }\
     .tnt_wood{\
         text-align:right;\
