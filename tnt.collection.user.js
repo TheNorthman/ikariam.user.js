b4c4a6e46d60d438bb6b9595c778792d929c33b5
Ronny Jespersen <git@rjj-net.dk>
2024-12-25 20:39:06 +0700

    v1.4.137

--- tnt.collection.user.js
+++ tnt.collection.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         TNT Collection
-// @version      1.4.136
+// @version      1.4.137
 // @namespace    tnt.collection
 // @author       Ronny Jespersen
 // @description  TNT Collection of Ikariam enhancements to enhance the game
@@ -24,7 +24,7 @@ function delay(time) {
 
 var tnt = {
 
-    version: "1.4.136", // GM_info.script.version,
+    version: "1.4.137", // GM_info.script.version,
 
     url: {
         versionUrl: "http://ikariam.rjj-net.dk/scripts/tnt.Collection/version.php",
@@ -105,7 +105,6 @@ var tnt = {
 
             // Resource
             tnt.resource.update();
-            tnt.resource.events();
 
             // Notification
             tnt.core.notification.init();
@@ -815,6 +814,9 @@ var tnt = {
                 table += '</table>';
 
                 $('#tnt_info_resources').html(table);
+
+                // Add events
+                tnt.resource.events();
             }
         },
 
