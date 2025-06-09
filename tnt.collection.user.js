65d94dcbde8ad130479cd94677dc1ca42d7d8728
Ronny Jespersen <github.com@rjj-net.dk>
2025-06-09 20:13:28 +0700

    v1.5.2

--- tnt.collection.user.js
+++ tnt.collection.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         TNT Collection
-// @version      1.5.1
+// @version      1.5.2
 // @namespace    tnt.collection
 // @author       Ronny Jespersen
 // @description  TNT Collection of Ikariam enhancements to enhance the game
